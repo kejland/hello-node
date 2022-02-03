0f3a34463ff93398df066deba9bb7a4d023bab65
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-02-03 15:18:02 -0500

    js file

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+console.log("\n");
+
+console.log("HH   HH        lll lll                                     lll      dd !!!");
+console.log("HH   HH   eee  lll lll  oooo      ww      ww  oooo  rr rr  lll      dd !!!");
+console.log("HHHHHHH ee   e lll lll oo  oo     ww      ww oo  oo rrr  r lll   ddddd !!!");
+console.log("HH   HH eeeee  lll lll oo  oo      ww ww ww  oo  oo rr     lll dd   dd    ");
+console.log("HH   HH  eeeee lll lll  oooo        ww  ww    oooo  rr     lll  dddddd !!!");
+
+console.log("\n");
+
+console.log("    ********");
+console.log("   **//////**");
+console.log(" **      //    ******   ******      /**");
+console.log("/**           **////** **////**  ******");
+console.log("/**     *****/**   /**/**   /** **///**");
+console.log("//**   ////**/**   /**/**   /**/**")
